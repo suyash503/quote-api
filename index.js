117ced1ab6bcc27ace27c6fc0d23d13872b7a02e
Suyash <suyashsingh2711@gmail.com>
2025-06-17 17:54:40 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+const quotes = [
+    { id: 1, text: "KEEP doing hard things daily" },
+    { id: 2, text: "KEEP your head down" },
+    { id: 3, text: "KEEP yourself at discomfort" }
+];
+
+app.get('/api/quotes', (req, res) => {
+    res.json(quotes); //req comes from the browser, res is the response that we give back back
+})
+
+app.get('/api/quotes/random', (req, res) => {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    res.json(randomQuote);
+});
+
+app.listen(3000, () => {
+    console.log("api server running at http://localhost:3000");
+});
